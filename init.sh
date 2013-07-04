c075af16e52d36de4f165ad43d3d120549235e07
Andreas Boesen <andreasb@selfnet.de>
2013-07-04 17:26:55 +0200

    fix

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.backup
 ln -snf `pwd`/vimrc ~/.vimrc
 ln -snf `pwd`/bashrc ~/.bashrc
 ln -snf `pwd`/tmux.conf ~/.tmux.conf
-ln -snf `pwd`/zshrc ~/.zshrc.local
+ln -snf `pwd`/zshrc.local ~/.zshrc.local
 ln -snf `pwd`/gitconfig ~/.gitconfig
 ln -snf `pwd`/screenrc ~/.screenrc
 ln -snf `pwd`/gntrc ~/.gntrc
